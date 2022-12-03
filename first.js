c53ffc5b5a3df22b1c8e51f1385bf146b5905939
Ashish Kumar <78853800+Ashish9836@users.noreply.github.com>
2022-12-03 17:50:35 +0530

    js updation

--- first.js
+++ first.js
@@ -6,3 +6,13 @@ Browser API
 -- Audio and Video API
 
 */
+// process.stdout.write never ends with a newline
+// process.stdout.write("Hey man!");
+// process.stdout.write("Hey man!")
+
+
+// alaways remember NaN is not equal to NaN
+// type of NaN is number
+// var a = 5,   memory bloack of value 5 is assigned to a
+// var b = a, similarty also here block assinged but remember b is having same black as having a 
+// b++,   it will create a new memory block of having value 5+1 = 6
